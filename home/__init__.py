f2783c533bc0ccdb3d1b727fcdc9a6360fe07968
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-07 17:41:13 +0530

    23318

---
+++ home/__init__.py
@@ -0,0 +1,16 @@
+body {
+    font.family: 'Segoe UI', sans_serif;
+    background_color; #f9f9f9;
+    color: #333
+    margin: 0;
+    padding: 0;
+    }
+    header {
+        background_color: #4CAFSO;
+        color: white;
+        padding: 20px;
+        text_align: center;
+        }
+        main {
+             padding: 20px;
+        }
