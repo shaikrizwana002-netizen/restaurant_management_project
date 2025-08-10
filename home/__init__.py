81fb8149781f7555c0e6d679999095c87343676c
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-10 17:07:27 +0530

    23325

--- home/__init__.py
+++ home/__init__.py
@@ -1,12 +1,26 @@
-settings.py
-RESTAURANT_PHONE = "+91_98709868977"
-
-
-(views.py)
-from django.conf import settings
-def home_view(request):
-    phone = settings.RESTAURANT_PHONE
-    return render(request, 'index.html', {'phone':phone})
-
-(index.html)    
-<p> call us: {{ phone}}</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTI_8">
+<title>Welcome to Sweet Heaven</title>
+<style>
+body {
+    font_family: Arial, sans_serif;
+    text_align: center;
+    padding_top: 50px;
+    background_color: #fdf6e3;
+}
+h1 {
+    color: #d35400;
+}
+p{
+    font_size: 1.2em;
+    color: #555;
+}
+</style>
+</head>
+<body>
+<h1>{{ restaurant_name }}</h1>
+<p>Welcome to our cozy corner of flavor and hospitality!</p>
+</body>
+</html>
