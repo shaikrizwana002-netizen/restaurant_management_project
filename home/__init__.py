8179eeee9285b9d7acd465ff4052ecf15a216af1
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-10 16:49:53 +0530

    23324

--- home/__init__.py
+++ home/__init__.py
@@ -1,35 +1,12 @@
-from django.shortcuts import render
+settings.py
+RESTAURANT_PHONE = "+91_98709868977"
 
-def menu_view(request):
-    menu_items = [
-        {'name': 'Marherita Pizza', 'price': '₹250'},
-        {'name': 'Paneer Tikka', 'price': '₹160'},
-        {'name': 'Masala Dosa', 'price': '₹120'},
-        {'name': 'Gulab Jamun', 'price': '₹80'},
-    ]
-    return render(request, 'menu.html', {'menu_items':menu_items})
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>Menu</title>
-    <style>
-    body { font_family: Arial, sans_serif; padding: 20px; }
-    h1 { color: #333;}
-    ul {list_style: none; padding:0;}
-    li { margin: 10px 0; padding: 10px; background: #f9f9f; border_radius: 5px; }
-    </style>
-    </head>
-    <body>
-    <h1>Perpex Bistro Menu</h1>
-    <ul>
-    {% for item in menu_items %}
-    <li><strong>{{ item.name}}</strng> _ {{ item.price}}</li>
 
-    from django.urls import path
-    from. views import menu_view
+(views.py)
+from django.conf import settings
+def home_view(request):
+    phone = settings.RESTAURANT_PHONE
+    return render(request, 'index.html', {'phone':phone})
 
-    urlpatterns = [
-        path('menu/', menu_view, name='menu'),
-    ]
-
-    menu_items = MenuItem.objects.all()
+(index.html)    
+<p> call us: {{ phone}}</p>
