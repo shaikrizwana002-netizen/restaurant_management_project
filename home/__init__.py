b640a59c6c76d57068a97c64a40835136594a3f5
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-10 15:45:28 +0530

    23320

--- home/__init__.py
+++ home/__init__.py
@@ -1,18 +1,27 @@
-# settings.py
-RESTAURANT_NAME = "YUMMY CHEESE BITE"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF_8">
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<header>
+<nav>
+<ul>
+</nav>
+</header>
 
-# view.py
-from django.conf import settings
-from django.shortcuts import render
+<main>
+<section class="about">
+<h1>Welcome to Perpex Bistro</h1>
+<pre><<At Perpex Bistro,we blend  tradition and innovation to serve unforgettable meals.our chefs each dish with care,using fresh,locally sourced ingredients.Welcome you'r dining solo or celebrating with friends,our warm ambiance and attentive service make every visit special.</p><pre>
+<pre><img src="images/restaurant_interior.jpg" alt="Index Perpex Bistro" style="width:100; max_width:600px;"<pre>
+<pre><img src="image/restaurant_exterior.jpg" alt="Perpex Bistro Exterior" style="width:100; max_width:600px;"<pre>
+</section>
+</main>
+<footer>
+<p>&copy; 2025 Perpex Bistro. All rights reserved.</p>
+</footer>
+</body>
+</html>
 
-def homepage(request):
-    context = {'restaurant_name': settings.RESTAURANT_NAME
-    }
-    return render(request, 'homepage.html', context)
-    <!__ homepage.html __>
-    <h1>Welcome to {{restaurant_name}}</h1>
-    # models.py
-    from django.db import models
-    class Restaurant(models.Model):
-        name = models.CharField(max_length=100)
-        
