cd75e67782047c51b4e9a0102621831ba3b5e1ca
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-18 10:34:48 +0530

    32573

--- home/__init__.py
+++ home/__init__.py
@@ -1,11 +1,11 @@
 <form id="contactForm">
-<label for="name">Name:</label>
-<input type="text"id="name" name="name" />
+ <label for="name">Name:</label>
+ <input type="text"id="name" name="name" />
 
-<label for="email">Email:</label>
-<input type="email" id="email" name="email" />
+ <label for="email">Email:</label>
+ <input type="email" id="email" name="email" />
 
-<button type="submit">Submit</button>
+ <button type="submit">Submit</button>
 </form>
 
 <script>
