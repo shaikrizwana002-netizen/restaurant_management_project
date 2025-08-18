25233137d005f8994dd0d34b233ea652006b15b3
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-18 13:21:24 +0530

    32568

--- home/__init__.py
+++ home/__init__.py
@@ -1,4 +1,4 @@
 <form style="text_align:center; margin:20px 0;">
-  <input type="text" placeholder="Search menu..." style="padding:8px; width
+  <input type="text" placeholder="Search menu..." style="padding:8px; width:200px;">
   <button type="submit" style="padding:8px;">Go</button>
 </form>
