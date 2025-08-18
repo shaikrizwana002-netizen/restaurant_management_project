a48d17e96475d3fea532a158041a02dfee5716d3
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-18 10:42:52 +0530

    32572

--- home/__init__.py
+++ home/__init__.py
@@ -1,21 +1,3 @@
-<form id="contactForm">
- <label for="name">Name:</label>
- <input type="text"id="name" name="name" />
-
- <label for="email">Email:</label>
- <input type="email" id="email" name="email" />
-
- <button type="submit">Submit</button>
-</form>
-
-<script>
-document.getElementById("contactForm").addEventListener("submit", function (event) {
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-
-    if (!name || !email) {
-        alert("Please fill in  both name and email field.");
-        event.preventDefault(); // Prevent form submission
-    }
-  });  
-</script>
+<footer>
+  <p>Opening Hours: Mon-Fri: 11am_9pm,Sat_Sun:10am-10pm</p>
+  </footer>
