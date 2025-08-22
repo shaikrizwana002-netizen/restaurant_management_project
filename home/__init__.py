b172a059970f2c68e326d4f7729ce4ea7cf65165
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-22 07:12:17 +0530

    37447

--- home/__init__.py
+++ home/__init__.py
@@ -1,4 +1,24 @@
-<form style="text_align:center; margin:20px 0;">
-  <input type="text" placeholder="Search menu..." style="padding:8px; width:200px;">
-  <button type="submit" style="padding:8px;">Go</button>
-</form>
+# menu/views.py
+from rest_framework.views import
+from rest_framework.response import
+
+class MenuView(APIView):
+    def get(self, request):
+        menu = [
+            {
+                "name": "Margherita pizza", 
+                "description": "Classical pizza with tomato sauce, mozzarella, and basil.",
+                "price": "8.99"
+                },
+                {
+                "name": "Caesar Salad",
+                "description": "Romaine lettuce eith Ceasar dressing, croutons, and parmesan.",
+                "price": 6.50
+                },
+                {
+                "name": "Spaghetti Carbonara",
+                "description": "Pasta with eggs, cheese, pancetta, and pepper",
+                "price": 10.75
+            }
+        ]     
+        return Response(menu)
