c310b3c9a96f21e8565124f96b44df839c5eb737
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-28 05:43:51 +0530

    39487

--- home/__init__.py
+++ home/__init__.py
@@ -1,10 +1,14 @@
-from django.db import models
-from django.contrib.auth.models import User
+# settings.py
+RESTAURANT_NAME = "Yummy Restauarnt"
 
-class Userfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models. CASCADE)
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=15)
-    def __str__(self):
-        return self.name
+# views.py
+from django.conf import settings
+from.django.shortcuts import render
+
+def homepage(request):
+    return render(request, 'homepage.html', {
+        'restaurant_name': settings.RESTAURANT_NAME
+    })
+
+    <!-- homepage.html -->
+    <h1>Welcome to {{ restaurant_name }}</h1>
