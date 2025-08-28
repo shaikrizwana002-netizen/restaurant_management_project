144529e0d0cb9cfabda8a6d4e04d431364919193
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-28 06:05:02 +0530

    39486

--- home/__init__.py
+++ home/__init__.py
@@ -1,14 +1,46 @@
-# settings.py
-RESTAURANT_NAME = "Yummy Restauarnt"
+# models.py
+from django.db import models
+
+class   modelsenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def --str--(self):
+        return self.name
 
 # views.py
-from django.conf import settings
-from.django.shortcuts import render
+from django.shortcuts import render
+from.models import modelsenuItem
+def menu_page(request):
+    items = MenuItem.objects.all()
+    return render(request, 'menu.html', {'menu_items': items})
 
-def homepage(request):
-    return render(request, 'homepage.html', {
-        'restaurant_name': settings.RESTAURANT_NAME
-    })
+    #urls.py
+    from django.urls import path
+    from . import views          
 
-    <!-- homepage.html -->
-    <h1>Welcome to {{ restaurant_name }}</h1>
+    urlpatterns = [ 
+        path('menu/', views.menu_page, name='menu'),
+    ]
+
+ <!-- templates/menu.html -->
+ <!DOCTYPE html>
+ <html>
+ <head>
+     <title>Menu</title>
+</head>
+<body>   
+    <h1>Our Menu</h1>
+    <ul>
+    {% for item in menu_items %}
+       <li>
+          <strong>{{item.name }}</strong> - ₹{{ item.price }}<br>
+          <em>{{ item.description }}</em>
+       </li>
+    {% empty %}
+       <li>No items available.</li>
+       {% render %}
+  </ul>
+</body>
+</html>      
