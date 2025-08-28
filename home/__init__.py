5eeadfd873352a20fd3408aca56cdd22ce44e2ab
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-28 08:51:28 +0530

    39483

--- home/__init__.py
+++ home/__init__.py
@@ -1,31 +1,31 @@
-# settings.py
-R ESTAURANT_ADDRESS = "123 Main Street, Eluru, Andhra Pradesh, India"
-
 # models.py
-class RestaurantInfo(models.Models):
+from django.db import models
+class ContactSubmission(models.Model)
     name = models.CharField(max_length=100)
-    address = models.CharField(max_length=255)
+    email = models.EmailField()
 
-# views.py
-from django.conf import settings
-from django.shortcuts import render    
+    def __str__(self):
+        return f"{self.naem} ({self.email})"
+
+# forms.py
+from django import forms
+from .models import ContactSubmission
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactSubmission
+        fields = ['name','email']
+
+
+from django.shortcuts import render,redirect
+from.forms import ContactForm
 
 def homepage(request):
-    address = settings.RESTAURANT_ADDRESS
-    return render(request, 'homepage.html', {'restaurant_address': address})
-
-
-<!--homepage.html -->
-<h2>Visit Us</h2>
-<p>{{ restaurant_address }}</p>
-
-<!-- Embedded Google Map -->
-<iframe
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876.123456789!2d81.123456!3d16.710123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a123456789abcdef%3A0xabcdef123456789!2s123%20Main%20Street%2C%20Eluru%2C%20Andhra%20Pradesh!5e0!3m2!1sen!2sin!4v1691234567890"
-  width="600"
-  height="450"
-  style="border:0;"
-  allowfullscreen=""
-  loading="lazy"
-  referrerpolicy="no-referrer-when-downgrade">
-  </iframe>
+    if request.method == 'POST':
+        from = ContactForm(request.POST)
+        if form.is_vaild():
+            form.save()
+            return redirect('homepage') # or show a success message
+    else:
+        form = ContactForm()
+        return render(request, 'homepage.html', {'form': form})        
