16dcc81d0ad206095d786c22825c107294a2a2c8
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-29 05:40:51 +0530

    39949

--- home/__init__.py
+++ home/__init__.py
@@ -1,48 +1,10 @@
-# models.py
 from django.db import models
-class ContactSubmission(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+
+class RestaurantLocation(models.Model):
+    address = models.CharField(max_length=100):
+    city = models.CharField(max=length=50):
+    state = models.CharField(max_length=15):
+    zip_code = models.CharField(max_length=10):
 
     def __str__(self):
-        return f"{self.naem} ({self.email})"
-
-# forms.py
-from django import forms
-from .models import ContactSubmission
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = ContactSubmission
-        fields = ['name','email']
-
-
-from django.shortcuts import render,redirect
-from.forms import ContactForm
-
-def homepage(request):
-    if request.method == 'POST':
-        from = ContactForm(request.POST)
-        if form.is_vaild():
-            form.save()
-            return redirect('homepage') # or show a success message
-    else:
-        form = ContactForm()
-        return render(request, 'homepage.html', {'form': form}) 
-
-
-# urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [ 
-    path('', views.homepage, name='homepage'),
-]
-
-<!-- homepage.html -->
-<h2>Contact Us</h2>
-<form method="post">
-   {%csrf-token%}
-   {{ form.as_p }}
-   <button type="submit">Submit</button>
-   </form>
+        return f"{self.address}, {self.city}, {self.state}, {self.zip_code}:
