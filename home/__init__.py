607365490ead38d09980c490c0faf0fde552de49
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-29 05:42:51 +0530

    39949

--- home/__init__.py
+++ home/__init__.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class RestaurantLocation(models.Model):
-    address = models.CharField(max_length=100):
-    city = models.CharField(max=length=50):
-    state = models.CharField(max_length=15):
-    zip_code = models.CharField(max_length=10):
+    address = models.CharField(max_length=100)
+    city = models.CharField(max=length=50)
+    state = models.CharField(max_length=15)
+    zip_code = models.CharField(max_length=10)
 
     def __str__(self):
-        return f"{self.address}, {self.city}, {self.state}, {self.zip_code}:
+        return f"{self.address}, {self.city}, {self.state}, {self.zip_code}"
