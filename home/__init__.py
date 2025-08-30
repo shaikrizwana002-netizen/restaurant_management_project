33a06b23dc2b5bea53ddd2b9d782f8c4a1b921d1
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-08-30 22:57:18 +0530

    39946

--- home/__init__.py
+++ home/__init__.py
@@ -7,3 +7,8 @@ class MenuItem(models.Model):
     image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
     def __str__(self):
         return  self.name
+
+ from django.contrib import admin
+ from django.urls import path,include
+ from django.conf import settings
+ from django.conf.urls.static import static       
