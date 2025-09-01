a56e3541de06083cb0278f8813130d1827a27b5a
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-01 09:15:40 +0530

    40952

--- home/__init__.py
+++ home/__init__.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Restaurant(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
