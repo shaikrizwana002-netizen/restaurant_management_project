0a88fcb9156dd033b9f3c4d7429aef62e024b175
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-01 08:53:21 +0530

    40953

--- home/__init__.py
+++ home/__init__.py
@@ -3,7 +3,7 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
-    opening_hours = models.JSONField(default=dict) # e.g., {"Monday": "9am_9pm,..."}
+    opening_hours = models.JSONField(null=True) # e.g., {"Monday": "9am_9pm,..."}
     def __str__(self):
         return  self.name
 
@@ -11,7 +11,7 @@ class Restaurant(models.Model):
  from home.models import Restaurant
         
 def homepage(request):
-    restaurant = Restaurant.object.restaurant = Restaurant.objects.first()  
+    restaurant = Restaurant.objects.first()  
     return render(request, 'homepage.html', {'restaurant': restaurant})       
 
 <!DOCTYPE html>
