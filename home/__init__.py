850839f8400736b58940a4515344c625baf53116
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-02 07:45:46 +0530

    41629

--- home/__init__.py
+++ home/__init__.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
+    logo = models.ImageField(upload_to='logos/')
     def __str__(self):
        return self.name
 
@@ -10,11 +10,15 @@ class Restaurant(models.Model):
 <!DOCTYPE.html>
 <html>
 <head>
-    <title>Restaurant Homepage/<title>
+    <title>{{ restaurant.name }}</title>
 </head>
 <body>
      <h1>Welcome to {{ restaurant.name }}</h1>
-     <p> Call us at: <strong>{{ restaurant.phone_number }}</strong></p>
+     {% if restaurant.logo %}
+        <img src="{{ restaurant.logo.url }}" alt="{{ restaurant.name }}" Logo style="max_width:200px;">
+    {% ielse %}
+         <p>No logo available.</p>
+    {% endif %}
 </body>
 </html>    
 
