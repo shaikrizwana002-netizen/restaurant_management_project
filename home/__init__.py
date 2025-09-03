8575c6e22e8cb15dab930b74bd36e7c612598c3d
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-03 09:05:25 +0530

    41626

--- home/__init__.py
+++ home/__init__.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-def homepage(request):
-    cart =request.session.get('cart', {})
-    total_items = sum(cart.values())
-    return render(request, 'homepage.html', {'total_items': total_items})
-
-
-def add-to-cart{request, item_id}:
-cart = request.session.get('cart'. {})
-cart[item_id] = cart.get(item_id, 0) + 1
-request.session['cart'] = cart
-return redirect('homepage')
+from django import forms
+ 
+class ContactForm(forms.Form):
+    email = forms.EmailField(
+        request=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Your email'})
+    )
+    message = forms.CharField(
+        required=True,
+        widget=forms.Textarea(attrs={'placeholder': 'Your email'})
+        min_length=10
+    ) 
