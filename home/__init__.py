730fd73e91232551620173e690debc9181add1ec
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-05 10:45:33 +0530

    42409

--- home/__init__.py
+++ home/__init__.py
@@ -1,44 +1,24 @@
-< !__ templates/faq.html __>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <title>FAQs</title>
-   <style>
-   body { font-family: Arial, sans-serif; padding: 20px; }
-   h1 { colo: #333; }
-   .faq-t-item { margin-bottom: 20px; }
-   .question { font-weight: bold; }
-   .answer { margin-left: 10px; }
-   </style>
-</head>
-<body>
-  <h1>Frequently Asked Question</h1>
-  <div class="faq-item">
-  <div class="question">Q: How do I reset my password?</div>
-  <div class="answer">A: Click on "Forget Password" at login and follow the instruction.</div>
-  </div>
-<div class="faq-item">
-  <div class="question">   Q: Can I edit my submission after uploading?</div>
-  <div class="answer">A: Yes, you can edit it until the .</div>
-  </div>
-</body>
-</html>  
+< div class="datetime">
+   <p>Current Date: {{ current-date }}</p>
+   <p>Current Time: {{ current-tome/format-time }}</p>
+   </div>
 
 from django.shortcuts import render
+from datetime import datetime
+def home-view(request):
+    now = datetime.now()
+    context = {
+        'current-date': now.date(),
+        'current-time': noe.time(),
+        }
+        return render(request, 'home.html', context)
 
-def faq-view(request):
-    return render(request, 'faq.html')
+from django import template
 
-from django.urls import path
-from.views import faq-view
+register = template.Library()
 
-from django.urls import path
-from .views import faq-view
-urlpatterns =[
-    path('faq/', faq-view, name='faq'),
-]
+@register.filter
+def format-time(value):
+    return value.strftime('%I:%M %p')  #,10:35 AM
+    {% load custom-filters %}
 
-<a href="{%  url 'faq' %}">FAQs</a>
-
-<a href="/faq/">FAQs</a>
