f71b112064efa0d32e48982b945ca462eadaaa9f
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-07 20:17:40 +0530

    42959

--- home/__init__.py
+++ home/__init__.py
@@ -1,54 +1,30 @@
-<!__ templates/base.html __>
+ # views.py
+ from django.shortcuts import render
+ import random
+
+ def order-confirmation(request):
+    # Simulate an order number for demonstration
+    order_number = random.randint(100000, 999999)
+    return render(request, 'order_confirmation.html', {'order_number': order_number})
+
+<!__ templates/order_confirmation __>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Order Confirmation</title>
+</head>
 <body>
-     {% block breadcrumbs %}
-     <! __Breadcrumbs will be injected here __>
-     {% endblock %}
-
-     {% block content %}
-     <!__Page_specific content __>
-     {% endblock %}
+    <h1> Order Confirmed!</h1>
+    <p>Thank You for your purchase</h1>.
+    <p><strong>Order Number:</strong>
 </body>
+</html>
 
-{% extends 'base.html' %}
+ # urls.py
+ from django.urls import path
+ from . import views
+ urlpatterns = [
+    path('order-confirmation/', views.order_confirmation, name='order_confirmation'),
 
-{% block breadcrumbs %}
-<nav aria-label="breadcrumb">
-   <ol>
-     <li><a href="{% url 'home" %} ">Home</a></li>
-   </ol>
-</nav>{% endblock content %}
-<h1>Welcome to Our Site</h1>
-{% endblock %}
-
-
-{% extends 'base.html' %}
-
-{% block breadcrumbs %}
-<nav aria-label="breadcrumb">
-   <ol>
-     <li>a href="{% url 'home' %}">Home</a></li>
-     <li>Menu</li>
-   </ol>  
-</nav>
-{% endblock %}
-
-{% block content %}
-<h1>Our Menu</h1>
-{% endblock %}
-
-{% extends 'base.html' %}
-
-{% block breadcrumbs %}
-<nav aria-label="breadcrumb">
-  <ol>
-    <li><a href="{% url 'home' %} <li><a href="{% url 'home' %}">Home</a></li>
-    <li><a href="{% url 'menu' %}">Menu</a></li>
-    <li>Order Confirmation</li>
-  </ol>
-</nav>
-{% endblock %}
-
-{% block content %}
-<h1>Thank You for Your Order!</h1>
-{% endblock %}
-
+ ]
