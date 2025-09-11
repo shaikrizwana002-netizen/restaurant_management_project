5ffd04eefca273e360d9264ad2037157fc39dcaa
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-11 07:23:36 +0530

    43555

--- home/__init__.py
+++ home/__init__.py
@@ -1,63 +1,14 @@
-from django.db import models
+class AboutUs(models.Model):
+    description = models.TextField()
+    image = models.ImageField(upload_to='about_us/')
 
-class Feedback(models.Model):
-    name = models.CharField(max-length)
-    feedback = model.TextField()
+def about_us_display(request):
+    about_us = AboutUs.objects.last()
+    return render(request, 'about_us_display.html', {'about_us': about_us})
 
-def __str__(self):
-    return f"Feedback from {self.name}"
-
-from django import forms
-from.models import Feedback
-
-class FeedbackForm(forms.ModelFrom)
-  class Meta:
-    model = Feedback 
-    fields = ['name', 'feedback']
-    widgets = {
-             'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'feedback': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
-}
-
-from django.shortcuts import render, redirect
-from .forms import FeedbackForm
-
-def submit_feedback(request):
-    if request.method == 'POST':
-        form = FeedbackForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('feedback_thank_you')
-    else:
-        form = FeedbackForm()
-    return render(request, 'feedback-form.html', {'form': form})
-
-def thank-you(request):
-    return render(request, 'thank-you.html')
-
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Submit Feedback</title>
-    <link rel="stylesheet" href="{% static 'css/style.css' %}">
-</head>
-<body>
-    <div class="container">
-        <h2>We'd love your feedback!</h2>
-        <form method="post">
-            {% csrf_token %}
-            {{ form.as_p }}
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-</body>
-</html>
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('feedback/', views.submit_feedback, name='submit_feedback'),
-    path('thank-you/', views.thank_you, name='feedback_thank_you'),
-]
+<div class="about_us_section">
+   {% if about_us.image %}
+     <img sr"{{ anbout_us.image.url }}" alt="Fusion Restaurant" style="max-width: 100%; border_radius: 8px;">
+     {% endif %}
+     <p>{{ about_us.description }}</p>
+</div>     
