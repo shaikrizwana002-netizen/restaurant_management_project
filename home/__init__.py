05fd9ac685bbd47fe33cba853f50d44af89cabb1
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-11 09:21:35 +0530

    43553

--- home/__init__.py
+++ home/__init__.py
@@ -1,70 +1,30 @@
 from django.db import models
 
-class MenuItem(models.Model):
+class TodaysSpecial(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-CART_SESSION_ID = 'cart'
+from django.shortcuts import render
+from .models import TodaysSpecial
 
-class Cart:
-    def __init__(self, request):
-        self.session = request.session
-        cart = self.session.get(CART_SESSION_ID)
-        if not cart:
-            cart = self.session[CART_SESSION_ID] = {}
-        self.cart = cart
+def homepage_view(request):
+    specials = TodaysSpecial.objects.all()
+    return render(request, 'homepage.html', {'specials': specials})
 
-    def add(self, item):
-        item_id = str(item.id)
-        if item_id not in self.cart:
-            self.cart[item_id] = {
-                'name': item.name,
-                'price': str(item.price),
-                'quantity': 1
-            }
-        else:
-            self.cart[item_id]['quantity'] += 1
-        self.save()
-
-    def save(self):
-        self.session.modified = True
-
-    def get_item(self):
-        return self.cart.values()
-
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import MenuItem
-from .cart import Cart
-
-def menu_list(request):
-    items = MenuItem.objects.all()
-    return render(request, 'menu_list.html', {'item': item})
-
-def add_to_cart(request, item_id):
-    item = get_object_or_404(MenuItem, id=item_id)
-    cart = Cart(request)
-    cart.add(item)
-    return redirect('view_cart')
-
-def view_cart(request):
-    cart = Cart(request)
-    return render(request, 'view_cart.html', {'cart_item': cart.get_item()})
-
-<h2>Menu</h2>
+<h2>Today's Specials</h2>
 <ul>
-  {% for item in items %}
+  {% for item in specials %}
     <li>
-      {{ item.name }} - ₹{{ item.price }}
-      <a href="{% url 'add_to_cart' item.id %}">Add to Cart</a>
+      <strong>{{ item.name }}</strong><br>
+      {{ item.description }}<br>
+      ₹{{ item.price }}
     </li>
+  {% empty %}
+    <li>No specials available today.</li>
   {% endfor %}
 </ul>
-<h2>Your Cart</h2>
-<ul>
-  {% for item in the cart_items %}
-    <li>{{ item.name }} - ₹{{ item.price }} x {{ item.quantity }}</li>
-  {% endfor %}
-  </ul>
+
