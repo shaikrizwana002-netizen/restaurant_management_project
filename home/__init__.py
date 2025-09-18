51bbce03e749d9ae74d06b24e5f881a002d87648
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-18 08:01:07 +0530

    45631

--- home/__init__.py
+++ home/__init__.py
@@ -1,15 +1,7 @@
 from django.db import models
 
-class OrderStatus(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
-
-class Order(models.Model):
-    customer_name = models,CharField(max_length=100)
-    order_data = models.DataTimeField(auto_now)
-    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
-
-    def __str__(self):
-        return f"Order #{self.id} - {self.customer_name}"
