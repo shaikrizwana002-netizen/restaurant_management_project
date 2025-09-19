8b73400f7a36ff64b3ff72e602e48e1f9bc5ccb3
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-19 08:44:45 +0530

    48234

--- home/__init__.py
+++ home/__init__.py
@@ -1,9 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Order(models.Model):
-    customer_name = models.CharField(max_length=100)
-    order_date = models.DateTimeField(auto_now=True)
-    status = models.ForeignKey('orders.OrderStatus', on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='orders')
+    date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_place=2)
 
-    def __str__(self):
-        return f"Order #{self.id} - {self.customer_name}"
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product_name = models.CharField(max_length=255)
+    quantity = models.PositiveIntgerField()
+    price = models.DecimalField(max_digits10, decimal_place=2)
+
+from rest_framework import serializers
+from .models import Order, OrderItem
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['product_name', 'quantity', 'price']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'date', 'total_price', 'items']
+
+from rest_framework import generics, permissions
+from .models import Order
+from .serializers import OrderSerializer
+
+class OrderHistoryView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user).order_by('-date')
