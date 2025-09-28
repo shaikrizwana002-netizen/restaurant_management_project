8bfc19629304ef2b801abdffa9cf422ae77ef417
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-09-28 07:46:12 +0530

    51563

--- home/__init__.py
+++ home/__init__.py
@@ -19,7 +19,7 @@ class ContactFormSubmissionSerializer(serializers.ModelSerializer):
 
 
 from rest_framework.generics import CreateAPIView
-from rest_framework.response import response 
+from rest_framework.response import Response 
 from rest_framework import status
 from .models import ContactFormSubmission
 from .serializers import ContactFormSubmissionSerializer
