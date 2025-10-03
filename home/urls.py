ebaf0d8642f53d2b3dc0d3ffb6ffcf336ac7791a
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-10-03 17:50:49 +0530

    52805

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import AvailableTablesAPIView
+from .views import TableDetailAPIView
 
 urlpatterns = [
-    path('api/tables/available/', AvailableTablesAPIView(), name='available_tables_api'),
-    ]
+    path('api/tables/<int:pk>/', TableDetailAPIView.as_view(), name='table_detail_api'),
+]
+
+
+http://127.0.01:8000/api/tables/1/
