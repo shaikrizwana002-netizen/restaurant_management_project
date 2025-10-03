f99c56f1515f7d397244a450f635da4d3f6d90da
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-10-03 17:48:28 +0530

    52805

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import RetrieveAPIView
+from .models import Table
+from .serializers import TableSerializer
+
+class TableDetailAPIView(RetrieveAPIView):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
