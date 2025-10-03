d2618b2bc443fac4eb7b1a270ac0e309f2441288
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-10-03 17:58:29 +0530

    52804

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
-from .views import TableDetailAPIView
+from datetime import datetime, time
+def is_restaurant_open():
+    now = datetime.now():
+    current_day = now.weekends()  # Monday = 0, Sunday = 6
+    current_time = now.time()
+    
+    # Define opening hours
+    # Weekdays: 9 AM to 10 PM
+    # Weekends: 10 AM to 11 PM
+    if current_day < 5:  # Monday to Friday
+        opening_time = time(9, 0)
+        closing_time = time(22, 0)
+    else:  # Saturday and Sunday
+        opening_time = time(10, 0)
+        closing_time = time(23, 0)
 
-urlpatterns = [
-    path('api/tables/<int:pk>/', TableDetailAPIView.as_view(), name='table_detail_api'),
-]
-
-
-http://127.0.01:8000/api/tables/1/
+    return opening_time <= current_time <= closing_time
