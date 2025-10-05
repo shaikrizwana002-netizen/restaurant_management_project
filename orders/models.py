0d75aa2ee067a0298190be52abeb308826c26720
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-10-05 22:00:06 +0530

    53377

--- orders/models.py
+++ orders/models.py
@@ -15,7 +15,7 @@ class Order(models.Model):
     def __str__(self):
         return f"Order #{self.id} - {self.customer_name}"
 
-        class OrderItem(models.Model):
+class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntgerField()
+    quantity = models.PositiveIntegerField()
