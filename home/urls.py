087b9b911bdd47d1d881e6af5543aec5b2d40996
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-10-05 21:45:37 +0530

    53378

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,6 @@
-from datetime import date
-from django.db.models import Sum
-from .models import Order
+from django.urls import path
+from .views import DailySpecialsView
 
-def get_daily_sales_total(target_date):
-   order = Orders.objects.filter(created_At_date=target_date)
-   total = orders.aggregate(total_sum=Sum('total_price'))['total_sum']
-   return total or 0   
+urlpatterns = [
+    path('daily-specials/', DailySpecialsView.as_view(), name='daily-specials'),
+]
