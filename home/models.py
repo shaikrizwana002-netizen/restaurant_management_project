70f654e5426417215cb6922feca077ca0d7a7a42
shaikrizwana002-netizen <shaik.rizwana002@gmail.com>
2025-10-14 18:37:31 +0530

    all changes

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
