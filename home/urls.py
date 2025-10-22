07c3baed3f3a7e24c498f676e83ec4365341fa15
rizwana <shaik.rizwana002@gmail.com>
2025-10-22 15:59:02 +0530

    commit all changes

--- home/urls.py
+++ home/urls.py
@@ -4,3 +4,6 @@ from .views import DailySpecialsView
 urlpatterns = [
     path('daily-specials/', DailySpecialsView.as_view(), name='daily-specials'),
 ]
+urlpatterns = [
+    path('menu/featured/', FeaturedMenuItemsView.as_view(), name='featured-menu-items'),
+]
