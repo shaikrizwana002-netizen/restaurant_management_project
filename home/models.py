a3f99ba5bd2a13561e914809efbdbf4bc61be74d
rizwana <shaik.rizwana002@gmail.com>
2025-10-22 14:23:44 +0530

    commit

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ class MenuCategory(models.Model):
     name = models.CharField(max_length=100)
 
 
+
 class MenuItem(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
