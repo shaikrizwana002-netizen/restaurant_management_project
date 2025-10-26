158c2ef2747f8ef3221d304fe4b44f57d9fa3197
rizwana <shaik.rizwana002@gmail.com>
2025-10-26 18:00:14 +0530

    commit all changes

--- orders/utils.py
+++ orders/utils.py
@@ -1,6 +1,7 @@
 import logging
 from django.core.exceptions import ObjectDoesNotExist
 from orders.models import Order  # Adjust this import based on your actual model location
+from home.models import MenuItem, get_distinct_cuisine
 
 logger = logging.getLogger(__name__)
 
@@ -29,3 +30,12 @@ def update_order_status(order_id, new_status):
         logger.exception(f"Unexpected error while updating order {order_id}: {e}")
         return False
 
+
+def get_distinct_cuisines():
+    """
+    Returns a list of all unique cuisine names currently used in menu items.
+    Useful for building dynamic filters or displaying cuisine options.
+    """
+    return list(
+        MenuItem.objects.values_list('cuisine__name', flat=True).distinct()
+    )
