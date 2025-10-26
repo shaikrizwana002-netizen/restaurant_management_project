df27fa978e9e1c9c6f8c697ce264781ce31fadb7
rizwana <shaik.rizwana002@gmail.com>
2025-10-26 16:58:59 +0530

    commit all changes

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from home.models import Restaurant
 
-# Create your tests here.
+class RestaurantInfoAPITest(APITestCase):
+    def test_get_restaurant_info(self):
+        # Step a: Create a sample Restaurant instance
+        restaurant = Restaurant.objects.create(
+            name='Test Restaurant',
+            address='123 Test St'
+        )
+
+        # Step b: Make a GET request to the API endpoint
+        response = self.client.get('/api/restaurant-info/')
+
+        # Step c: Assert that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+        # Step d: Assert that the response data matches the sample restaurant
+        self.assertEqual(response.data[0]['name'], restaurant.name)
+        self.assertEqual(response.data[0]['address'], restaurant.address)
